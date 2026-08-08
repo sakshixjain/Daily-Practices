48c58cffb46f5dbded9d9da151beffbe1dfcb74f
Sakshi Jain <sakshijainjain36@gmail.com>
2026-08-08 09:09:07 +0530

    added star pattern

---
+++ STAR_PATTERN/rightAnglePattern.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     int n; 
+//     cout<<" enter the number "<<endl;
+//     cin>>n;
+
+//     for(int i=0; i<n; i++){
+//         for( int j=0; j<=i; j++){
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+//OUTPUT 
+//  * 
+//  *  * 
+//  *  *  * 
+//  *  *  *  * 
+
+
+int main(){
+    int n; 
+    cout<<" enter the number "<<endl;
+    cin>>n;
+
+    // for(int i=0; i<n; i++){
+    //     for( int j=n-1; j>=i; j--){
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+// OUTPUT
+
+//  *  *  *  * 
+//  *  *  * 
+//  *  * 
+//  * 
+
+     for(int i=0; i<n; i++){
+            cout<<" - ";
+        for( int j=n-1; j>=i; j--){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+
+
+
+}
