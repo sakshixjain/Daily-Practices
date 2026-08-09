8adc7eac9e061d273ff41538ede995d207bc9dfa
Sakshi Jain <sakshijainjain36@gmail.com>
2026-08-09 23:41:21 +0530

    commit

--- STAR_PATTERN/rightAnglePattern.cpp
+++ STAR_PATTERN/rightAnglePattern.cpp
@@ -44,14 +44,49 @@ int main(){
 //  *  * 
 //  * 
 
-     for(int i=0; i<n; i++){
-        for( int j=n-1; j>=i; j--){
-            cout<<"- ";
-        }
-        for(int k=0; k<=i; k++){
-            cout<<" *";
-        }
-        
-        cout<<endl;
-    }
+    //  for(int i=0; i<n; i++){
+    //     for( int j=n-1; j>=i; j--){
+    //         cout<<"  ";
+    //     }
+    //     for(int k=0; k<=i; k++){
+    //         cout<<" *";
+    //     }
+
+    //     cout<<endl;
+    // }
+
+    //OUTPUT
+
+
+//            *
+//          * *
+//        * * *
+//      * * * *
+//    * * * * *
+
+
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<i; j++ ){
+//         cout<<" -";
+//     }
+//     for(int k=n-1; k>=i; k--){
+//         cout<<" *";
+//     }
+// cout<<endl;
+// }
+
+
+//OUTPUT
+
+
+//  * * * * *
+//  - * * * *
+//  - - * * *
+//  - - - * *
+//  - - - - *
+
+
+
 }
+
+
--- STAR_PATTERN/squarePattern.cpp
+++ STAR_PATTERN/squarePattern.cpp
@@ -8,9 +8,18 @@ int main(){
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            cout<<" * ";
+            cout<<j+1<< " ";
         }
         cout<<endl;
     }
     return 0;
+
+    
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+
+
 }
