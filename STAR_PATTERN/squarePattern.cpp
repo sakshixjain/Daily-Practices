ee351766161fb0103d12881235865cf5dade15bc
Sakshi Jain <sakshijainjain36@gmail.com>
2026-08-10 09:03:34 +0530

    hollow star pattern

--- STAR_PATTERN/squarePattern.cpp
+++ STAR_PATTERN/squarePattern.cpp
@@ -6,13 +6,12 @@ int main(){
     cout<<"Enter the number "<<endl;
     cin>>n;
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout<<j+1<< " ";
-        }
-        cout<<endl;
-    }
-    return 0;
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout<<j+1<< " ";
+    //     }
+    //     cout<<endl;
+    // }
 
     
 // * * * * * 
@@ -22,4 +21,34 @@ int main(){
 // * * * * * 
 
 
+
+// for(int i=1; i<=n; i++){
+//     for(int j=1; j<=i; j++){
+//         if(j==1 || j==i || i==n){
+//             cout<<"*";
+//         }else{
+//             cout<<" ";
+//         } 
+        
+//     }
+//     cout<<endl;
+// }
+
+// *
+// **
+// * *
+// *  *
+// *****
+
+
+for(int i=1; i<=n; i++){
+    for(int j=1; j<=n; j++){
+        if(j==1 ||j==n || i==1 || i==n){
+            cout<<"*";
+        }else{
+            cout<<" ";
+        } 
+    }
+    cout<<endl;
+}
 }
