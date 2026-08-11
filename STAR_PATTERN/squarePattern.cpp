92e043df04d19da62e1faa8e015920c37dedc3e2
Sakshi Jain <sakshijainjain36@gmail.com>
2026-08-11 09:10:41 +0530

    commit

--- STAR_PATTERN/squarePattern.cpp
+++ STAR_PATTERN/squarePattern.cpp
@@ -107,14 +107,45 @@ int main(){
 //     **
 //      *
 
-for(int i=0; i<n; i++){
-    for(int j=0; j<n-i; j++){
-       cout<<" ";
-    }
-    for(int k=0; k<2*i-1; k++){
-       cout<<"*";
-    }
-    cout<<endl;
-}
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<n-i; j++){
+//        cout<<" ";
+//     }
+//     for(int k=0; k<2*i-1; k++){
+//        cout<<"*";
+//     }
+//     cout<<endl;
+// }
+
+
+
+
+// for(int i=0; i<n; i++){
+//             for(int j=0; j<i; j++){
+//                 cout<<"*";
+//             }
+//             cout<<endl;
+//         }
+//             for(int i=n-1; i>=0; i--){
+//                 for(int k=0; k<i; k++){
+            
+//                 cout<<"*";
+            
+//             }
+          
+//             cout<<endl;
+//         }
+
+
+//         *
+//         **
+//         ***
+//         ****
+//         *****
+//         *****
+//         ****
+//         ***
+//         **
+//         *
 
 }  
