13b7286539e03920c0b85a6258fbc3b8286acbf7
Sakshi Jain <sakshijainjain36@gmail.com>
2026-08-13 09:06:24 +0530

    commit

--- RECURSION/printTable.cpp
+++ RECURSION/printTable.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void printTable(int n, int i){
-      if(i>10) return;
+// void printTable(int n, int i){
+//       if(i>10) return;
 
-    cout<<""<<n<<"*"<<i<<" = "<<n*i<<endl;
+//     cout<<""<<n<<"*"<<i<<" = "<<n*i<<endl;
 
-    return printTable(n, i+1);
+//     return printTable(n, i+1);
 
-}
+// }
 
 int main(){
     int n;
     cout<<"enter the number";
     cin>>n;
 
-    printTable(n,1);
+    // printTable(n,1);
+int i=1;
+    while(i<=10){
+        cout<<n*i<<endl;
+        i++;
+    }
+    
 }
